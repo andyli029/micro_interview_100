edb2bc58f50a98d3439cce3160189b8be9ee3b97
lizhiang <zhiangli029@gmail.com>
2017-05-11 04:13:21 +0800

    add 20.c

--- 20/20.c
+++ 20/20.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
 
+int str2int(char *str)
+{
+    char *cursor = str;
+    int num = 0;
+    int sum = 0;
+
+    for (cursor; (*cursor >= '0'  && *cursor <= '9'); cursor++)
+    {
+        num = (int)( *cursor - '0');
+
+        if ( sum == 0)
+        {
+            sum = num;
+        }  
+        else
+        {
+            sum = sum * 10 + num;
+        }  
+    }  
+
+    return sum;
+}
+
+int main()
+{
+    char num_str[10] = {"34567"};
+
+    printf("%d\n", str2int(num_str));
+
+    return 0;
+}
 
 
