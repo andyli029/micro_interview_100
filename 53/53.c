09e4abd5a7ea77132894859df2f06cc2255dca47
lizhiang <zhiangli029@gmail.com>
2017-05-30 17:10:23 +0800

    add Combination in 53

--- 53/53.c
+++ 53/53.c
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 //函数功能 ： 求一个字符串某个区间内字符的全排列
@@ -54,11 +55,45 @@ void Permutation(char *pStr)
 	//Permutation_Solution2(pStr, pStr);
 }
 
+void Combination_m(char *pStr, int m, vector<char> &result)  
+{  
+    if(pStr == NULL || (*pStr == '\0'&& m != 0))  
+        return;  
+    if(m == 0) //递归终止条件  
+    {  
+        for(unsigned i = 0; i < result.size(); i++)  
+            cout<<result[i];  
+        cout<<endl;  
+        return;  
+    }  
+    //选择这个元素  
+    result.push_back(*pStr);  
+    Combination_m(pStr + 1, m - 1, result);  
+    result.pop_back();  
+    //不选择这个元素  
+    Combination_m(pStr + 1, m, result);  
+}  
+//函数功能 ： 求一个字符串的组合  
+//函数参数 ： pStr为字符串  
+//返回值 ：   无  
+void Combination(char *pStr)  
+{  
+    if(pStr == NULL || *pStr == '\0')  
+        return;  
+    int number = strlen(pStr);  
+    for(int i = 1; i <= number; i++)  
+    {  
+        vector<char> result;  
+        Combination_m(pStr, i, result);  
+    }  
+}  
+
 int main()
 {
     char str[] = "abc";
         
-    Permutation(str);
+    //Permutation(str);
+    Combination(str);    
 
     return 0;
 }
