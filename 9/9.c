7bf227410a71f9886d654cbd5d7b2a08e88779ec
lizhiang <zhiangli029@gmail.com>
2017-07-02 10:55:20 +0800

    add be_post in the array

--- 9/9.c
+++ 9/9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// the bad taste
 bool desideTree(int* a, int start, int end)
 {
 	if(start + 1 >= end)
@@ -31,11 +32,61 @@ bool desideTree(int* a, int start, int end)
 }
 
 
+// my code: the damn onsite in 2017.6.30 on the 2nd round of AI&R in MS, 
+// a long long ..way to go
+bool be_post(int arr[], int length)
+{
+	int i = 0;
+	int mid = 0;
+
+	if (arr == NULL)
+		return false;
+
+	if (length == 1 || length == 0)// the two  choice: or judge pre the func
+		return true;
+	
+	int root = arr[length - 1];
+	
+	for (i = 0; i < length; i++)
+	{
+		if (arr[i] > root)
+		{
+			mid = i;
+			break;
+		}
+
+		mid = i; // the past bugfix 
+	}
+
+	i = mid;
+	for (i; i < length - 1; i++)
+	{
+		if (arr[i] <= root)
+		{
+			return false;
+		}
+	}
+
+	bool left = true;
+	//if (mid > 0) // the past bugfix
+		left = be_post(arr, mid);		
+
+	bool right = true;
+	//if (mid < length - 1)// the past bugfix
+		right = be_post(arr + mid, length - mid - 1);	
+
+	return (left && right);
+}
+
+
 int main()
 {
 	int a[4] = {7, 4, 6, 5};
 
-	bool result = desideTree(a, 0, 3);
+	int a1[4] = {1, 2, 3, 4};
+	//bool result = desideTree(a, 0, 3);
+
+    bool result = be_post(a1, 4);
 
 	if(result)
 		printf("true\n");
